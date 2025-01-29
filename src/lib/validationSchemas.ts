e0b5acc9c738f5dfd83e5b34fb416797a8e53b21
Senyr <102770968+Senyr@users.noreply.github.com>
2025-01-28 20:02:31 -1000

    Added an Input Finances page

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -5,6 +5,8 @@ export const AddStuffSchema = Yup.object({
   quantity: Yup.number().positive().required(),
   condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
   owner: Yup.string().required(),
+  income: Yup.number().required(),
+  expenses: Yup.number().required(),
 });
 
 export const EditStuffSchema = Yup.object({
