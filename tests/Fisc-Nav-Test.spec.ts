d527256aa8bec6622159e85b6e3419b28938eb23
Hayden Bireley <hbireley@hawaii.edu>
2025-02-14 10:20:20 -1000

    updates

--- tests/Fisc-Nav-Test.spec.ts
+++ tests/Fisc-Nav-Test.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  storageState: 'ses-auth.json',
-});
+// test.use({
+//  storageState: 'ses-auth.json',
+// });
 
 test('Change Password', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.reload();
+  await page.goto('http://localhost:3000/auth/signin');
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('john@foo.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('changeme');
+  await page.waitForTimeout(1500);
+  await page.getByRole('button', { name: 'Sign In' }).click();
   await page.waitForTimeout(1500);
   await page.getByRole('button', { name: 'john@foo.com' }).click();
   await page.getByRole('link', { name: 'Change Password' }).click();
@@ -20,10 +25,15 @@ test('Change Password', async ({ page }) => {
 });
 
 test('analyst dashboard and input financial information page', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.reload();
+  await page.goto('http://localhost:3000/auth/signin');
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('john@foo.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('changeme');
   await page.waitForTimeout(1500);
-  await page.goto('http://localhost:3000/analyst');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.waitForTimeout(1500);
+  await page.getByRole('link', { name: 'ANALYST' }).click();
   await expect(page.getByRole('heading', { name: 'Analyst Dashboard' })).toBeVisible();
   await page.waitForTimeout(1500);
   await page.getByRole('link', { name: 'INPUT FINANCIALS' }).click();
