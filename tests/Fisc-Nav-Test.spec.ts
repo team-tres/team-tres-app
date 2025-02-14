3b3cb59b880ba5267dd2d58c502a14d666809166
Hayden Bireley <hbireley@hawaii.edu>
2025-02-14 00:08:10 -1000

    Finish with the test cases for working pages

--- tests/Fisc-Nav-Test.spec.ts
+++ tests/Fisc-Nav-Test.spec.ts
@@ -4,26 +4,28 @@ test.use({
   storageState: 'ses-auth.json',
 });
 
-test.only('Change Password', async ({ page }) => {
+test('Change Password', async ({ page }) => {
   await page.goto('http://localhost:3000/');
+  await page.reload();
+  await page.waitForTimeout(1500);
   await page.getByRole('button', { name: 'john@foo.com' }).click();
-  await page.waitForTimeout(2500);
   await page.getByRole('link', { name: 'Change Password' }).click();
   await expect(page.getByRole('heading', { name: 'Change Password' })).toBeVisible();
+  await page.waitForTimeout(1500);
   await page.getByRole('button', { name: 'john@foo.com' }).click();
-  await page.waitForTimeout(2500);
+  await page.waitForTimeout(1000);
   await page.getByRole('link', { name: 'Sign Out' }).click();
   await expect(page.getByRole('heading', { name: 'Do you want to sign out?' })).toBeVisible();
-  await page.waitForTimeout(2500);
   await page.getByRole('button', { name: 'Sign Out' }).click();
 });
 
 test('analyst dashboard and input financial information page', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await page.waitForTimeout(2000);
-  await page.getByRole('link', { name: 'Input Financials' }).click();
-  await expect(page.getByRole('heading', { name: 'Input Financials' })).toBeVisible();
-  await page.waitForTimeout(2000);
-  await page.getByRole('link', { name: 'Analyst' }).click();
+  await page.reload();
+  await page.waitForTimeout(1500);
+  await page.goto('http://localhost:3000/analyst');
   await expect(page.getByRole('heading', { name: 'Analyst Dashboard' })).toBeVisible();
+  await page.waitForTimeout(1500);
+  await page.getByRole('link', { name: 'INPUT FINANCIALS' }).click();
+  await expect(page.getByRole('heading', { name: 'Input Financials' })).toBeVisible();
 });
--- tests/setup-session.spec.ts
+++ tests/setup-session.spec.ts
@@ -1,19 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeAll(async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.waitForTimeout(2500);
-});
-
 test('Setup session', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: 'Login' }).click();
-  await page.waitForTimeout(2500);
   await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.locator('input[name="email"]').focus();
-  await page.locator('input[name="email"]').fill('john@foo.com');
-  await page.locator('input[name="password"]').fill('changeme');
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('john@foo.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('changeme');
+  await page.waitForTimeout(1500);
   await page.getByRole('button', { name: 'Sign In' }).click();
-  await expect(page.getByRole('heading', { name: 'Stuff' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Welcome john@foo.com' })).toBeVisible();
+  // await page.context().clearPermissions();
+  // await page.context().clearLocalStorage();
   await page.context().storageState({ path: 'ses-auth.json' });
 });
