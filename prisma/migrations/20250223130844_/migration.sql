89fb31c0c2a55ae7ebb77817128252d1e25e4ce3
Ethan Christman <ec72@hawaii.edu>
2025-02-26 08:42:36 -1000

    finished both issues 21/22
    Was able to connect both admin dashboard pages and admin user management pages to the database; Buttons do not actually function to make changes to the database yet - future fixes

--- prisma/migrations/20250223130844_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `totalAssets` to the `FinancialCompilation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "FinancialCompilation" ADD COLUMN     "totalAssets" INTEGER NOT NULL;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row, Table, Button, ButtonGroup } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
 import { adminProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import Link from 'next/link';
+import getUsers from '../queries/admin/getUser';
 
 const AdminPage = async () => {
   const session = await getServerSession(authOptions);
@@ -13,8 +13,8 @@ const AdminPage = async () => {
       user: { email: string; id: string; randomKey: string };
     } | null,
   );
-  const users = await prisma.user.findMany();
-
+  const users = await getUsers();
+  const pendingUsers = users.filter(user => user.status === true);
   return (
     <main>
       <Container id="dashboard" fluid className="py-3">
@@ -37,32 +37,38 @@ const AdminPage = async () => {
         <Row>
           <Col>
             <h2>Pending Users</h2>
-            <Table striped bordered hover>
-              <thead>
+            <Table striped bordered hover className="shadow bg-white">
+              <thead className="bg-secondary text-white">
                 <tr>
-                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Role</th>
                   <th>Email</th>
-                  <th>Set Role</th>
-                  <th>Set Company</th>
                   <th>Status</th>
                   <th>Actions</th>
                 </tr>
               </thead>
               <tbody>
-                {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.email}</td>
-                    <td>__________</td>
-                    <td>__________</td>
-                    <td style={{ backgroundColor: '#FFF3CD' }}>Pending</td>
-                    <td>
-                      <Button variant="success" size="sm">Allow</Button>
-                      {' '}
-                      <Button variant="danger" size="sm">Deny</Button>
+                {pendingUsers.length > 0 ? (
+                  pendingUsers.map((user) => (
+                    <tr key={user.email}>
+                      <td>{user.username}</td>
+                      <td>{user.role}</td>
+                      <td>{user.email}</td>
+                      <td style={{ backgroundColor: '#FFF3CD' }}>Pending</td>
+                      <td>
+                        <Button className="btn btn-sm btn-success">Allow</Button>
+                        {' '}
+                        <Button className="btn btn-sm btn-danger">Deny</Button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={5} className="text-center">
+                      No pending users found.
                     </td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </Table>
           </Col>
