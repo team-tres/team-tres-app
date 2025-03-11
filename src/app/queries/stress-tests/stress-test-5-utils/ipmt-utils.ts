f633417e2257c129d7b83e4cb776bd88b12f83c0
Jayden Ontalan Capistrano <jaydenc8@hawaii.edu>
2025-03-11 08:36:17 -1000

    Added TSDoc and single-line comments.

--- src/app/queries/stress-tests/stress-test-5-utils/ipmt-utils.ts
+++ src/app/queries/stress-tests/stress-test-5-utils/ipmt-utils.ts
@@ -1,5 +1,13 @@
 import { MONTHS_IN_YEAR } from '@config/constants';
 
+/**
+ * Calculates the interest payment for a given loan amount based on the annual interest rate.
+ * The function assumes the interest rate is provided as an annual rate and calculates the interest for one month.
+ *
+ * @param loanAmount The total loan amount
+ * @param annualInterestRate The annual interest rate of the loan, represented as a decimal (e.g., 6% is 0.06)
+ * @returns The interest payment for a single month based on the provided loan amount and annual interest rate
+ */
 export default function calculateInterestPayment(loanAmount: number, annualInterestRate: number): number {
   const monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
 
--- src/app/queries/stress-tests/stress-test-5-utils/loan-generation.ts
+++ src/app/queries/stress-tests/stress-test-5-utils/loan-generation.ts
@@ -2,6 +2,15 @@ import { MONTHS_IN_YEAR } from '@config/constants';
 import { calculateLoan, LoanCalculatorInput, LoanCalculatorOutput } from './loan-utils';
 import calculateInterestPayment from './ipmt-utils';
 
+/**
+ * Generates an array of loan balances by year, based on the loan amount, interest rate, and loan period.
+ * Calculates the yearly loan balances by taking into account the interest accumulated and the yearly payments.
+ *
+ * @param loanAmount The total loan amount
+ * @param interestRate The annual interest rate of the loan, represented as a decimal (e.g., 6% is 0.06)
+ * @param loanPeriod The loan period in years
+ * @returns An array of loan balances for each year, including interest accumulation and payments
+ */
 export default function generateLoanBalances(loanAmount: number, interestRate: number, loanPeriod: number): number[] {
   const input: LoanCalculatorInput = {
     loanAmount,
@@ -9,21 +18,25 @@ export default function generateLoanBalances(loanAmount: number, interestRate: n
     loanPeriod,
   };
 
+  // Get the loan details from the loan-utils module
   const loanDetails: LoanCalculatorOutput = calculateLoan(input);
   let balance = input.loanAmount;
   let newBalance = balance;
   const yearlyPayment = loanDetails.monthlyPayment * MONTHS_IN_YEAR;
   const loanBalances: number[] = [];
 
+  // Loop through each year to calculate the subsequent balances
   for (let year = 0; year <= input.loanPeriod; year++) {
     const interestEarned = calculateInterestPayment(balance, input.interestRate) * MONTHS_IN_YEAR;
 
     newBalance = balance + interestEarned;
 
+    // newBalance = presentBalance + interest - yearlyPayment
     balance += interestEarned - yearlyPayment + loanDetails.interestPerPayment[year];
 
     loanBalances.push(newBalance);
 
+    // Loan is paid off
     if (balance <= 0) {
       break;
     }
