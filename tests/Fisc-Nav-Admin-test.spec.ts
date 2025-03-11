a7ade06f385ebccf803cfa6308aa7620b6da60f7
Hayden Bireley <hbireley@hawaii.edu>
2025-03-11 00:04:12 -1000

    updated github actions

--- tests/Fisc-Nav-Admin-test.spec.ts
+++ tests/Fisc-Nav-Admin-test.spec.ts
@@ -1,20 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-// test.use({
-//  storageState: 'admin-auth.json'
-// });
+test.use({
+  storageState: 'admin-auth.json'
+});
 
 test('Admin tests', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByLabel('Email').click();
-  await page.getByLabel('Email').fill('admin@gmail.com');
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('password');
-  await page.waitForTimeout(1500);
-  await page.getByRole('button', { name: 'Sign In' }).click();
-  await expect(page.getByRole('link', { name: 'ADMIN' })).toBeVisible();
   await page.waitForTimeout(1500);
   await page.getByRole('link', { name: 'ADMIN' }).click();
   await expect(page.getByRole('heading', { name: 'Admin Dashboard' })).toBeVisible();
