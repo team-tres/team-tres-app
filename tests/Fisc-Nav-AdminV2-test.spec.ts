cc19c97e398e5bf3e4aaae59330bb1fa10834ae9
Hayden Bireley <hbireley@hawaii.edu>
2025-03-11 15:18:05 -1000

    Update Fisc-Nav-AdminV2-test.spec.ts

--- tests/Fisc-Nav-AdminV2-test.spec.ts
+++ tests/Fisc-Nav-AdminV2-test.spec.ts
@@ -10,6 +10,7 @@ test('Admin test', async ({ page }) => {
   await page.getByLabel('Password').fill('password');
   await page.waitForTimeout(1500);
   await page.getByRole('button', { name: 'Sign In' }).click();
+  await page.waitForTimeout(2000);
   await page.getByRole('link', { name: 'ADMIN' }).click();
   await expect(page.getByRole('heading', { name: 'Admin Dashboard' })).toBeVisible();
 });
