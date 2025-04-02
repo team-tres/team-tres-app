9f151b887f2d57b7347482b212d63b4ecaf8adbe
Jayden Ontalan Capistrano <jaydenc8@hawaii.edu>
2025-04-02 12:49:49 -1000

    Setting up to validate finanaical calculations
    - installed vitest ^3.1.1 for easier test handling
      - installed to dev dependencies instead of production dependencies
    - minor folder structure file renaming for better reflection on use cases

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: [
+      'src/app/queries/stress-tests/**/*.test.ts',
+      'src/app/queries/forecasts/**/*.test.ts',
+      'src/app/queries/financial-comp/**/*.test.ts',
+    ],
+  },
+});
