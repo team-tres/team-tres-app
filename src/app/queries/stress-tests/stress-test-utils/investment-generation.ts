06a7aa67badc9fcad03d771baa2ba0c47e4ac76d
Jayden Ontalan Capistrano <jaydenc8@hawaii.edu>
2025-04-05 18:17:16 -1000

    Implementing ReinvestmentPercentage and impactedYears into Investment Logic
    Scope:
    
    `src/app/queries/stress-tests/utils/investment-generation.ts`
    
    Summary:
    - implemented the use of how much can be reinvested
    - taking into account how long there would be a reinvestment
    
    Notes:
    - the `reinvestmentPercentage` is clamped in-case of bad input

--- src/app/queries/stress-tests/stress-test-utils/investment-generation.ts
+++ src/app/queries/stress-tests/stress-test-utils/investment-generation.ts
@@ -7,15 +7,23 @@ export interface InvestmentDetails {
   impactedYears: number;
   reinvestmentPercentage: number;
 }
-const generateInvestmentBalances = ({ investmentAmount, interestRate }: InvestmentDetails) => {
+const generateInvestmentBalances = ({
+  investmentAmount,
+  interestRate,
+  impactedYears,
+  reinvestmentPercentage,
+}: InvestmentDetails) => {
   let interestEarned = 0;
   const investmentBalances: number[] = [];
   let balance = investmentAmount;
+  const clampedReinvestment = Math.min(1, Math.max(0, reinvestmentPercentage));
 
   for (let forecastedYears = 0; forecastedYears < MAX_FORECAST_SIZE; forecastedYears++) {
-    interestEarned = calculateInterestPayment(balance, interestRate, 'yearly');
+    interestEarned = calculateInterestPayment(balance, interestRate, 'yearly') * clampedReinvestment;
 
-    balance += interestEarned;
+    if (forecastedYears < impactedYears) {
+      balance += interestEarned;
+    }
     investmentBalances.push(balance);
   }
   return investmentBalances;
