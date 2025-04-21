82f3c85992fd0f9f76422fbbb98e80c10369c15e
Phat Ca <phatca@hawaii.edu>
2025-04-20 23:33:07 -1000

    updated model stresstest

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,6 +11,7 @@ async function main() {
       role: Role.CLIENT,
       companyIni: 'Company 1',
       status: true,
+      companyId: 1,
     },
     { email: 'admin@gmail.com',
       username: 'admin',
@@ -24,7 +25,7 @@ async function main() {
       role: Role.ANALYST,
       companyIni: 'Company 1',
       status: true,
-
+      companyId: 1,
     },
     {
       email: 'auditor@gmail.com',
@@ -33,7 +34,7 @@ async function main() {
       role: Role.AUDITOR,
       companyIni: 'Company 1',
       status: true,
-
+      companyId: 1,
     },
   ];
 
@@ -44,27 +45,7 @@ async function main() {
 
   /* eslint-disable no-await-in-loop */
   //* * Seeding logic */
-  for (const user of users) {
-    user.password = await hash(user.password, 10);
-    await prisma.user.upsert({
-      where: { email: user.email },
-      update: {
-        password: user.password,
-        username: user.username,
-        role: user.role,
-        status: user.status,
-        companyIni: user.companyIni,
-      },
-      create: {
-        email: user.email,
-        username: user.username,
-        password: user.password,
-        role: user.role,
-        status: user.status,
-        companyIni: user.companyIni,
-      },
-    });
-  }
+  
 
   for (const company of companies) {
     await prisma.company.upsert({
@@ -75,36 +56,59 @@ async function main() {
       },
     });
   }
+for (const user of users) {
+    user.password = await hash(user.password, 10);
+    await prisma.user.upsert({
+      where: { email: user.email },
+      update: {
+        password: user.password,
+        username: user.username,
+        role: user.role,
+        status: user.status,
+        companyIni: user.companyIni,
+        companyId: user.companyId,
+      },
+      create: {
+        email: user.email,
+        username: user.username,
+        password: user.password,
+        role: user.role,
+        status: user.status,
+        companyIni: user.companyIni,
+        companyId: user.companyId,
 
-  // Seed StressTest model with dummy data
-  const company1 = await prisma.company.findUnique({ where: { name: 'Company 1' } });
-  const adminUser = await prisma.user.findUnique({ where: { email: 'admin@gmail.com' } });
-
-  if (company1 && adminUser) {
-    await prisma.stressTest.create({
-      data: {
-        companyId: company1.id,
-        userId: adminUser.id,
-        investmentAmount: 1000000,
-        interestRate: new Prisma.Decimal(5),
-        interestRateDrop: new Prisma.Decimal(2),
-        impactedYears: 5,
-        reinvestmentPercentage: new Prisma.Decimal(0.02),
-
-        investmentRate: new Prisma.Decimal(0.03),
-        investmentRateDrop: new Prisma.Decimal(0.01),
-
-        expensesAndYear: JSON.stringify({ year2023: 50000, year2024: 55000 }),
-        increasePercentage: new Prisma.Decimal(0.05),
-
-        loanPeriod: 10,
-        baselineInterestRate: new Prisma.Decimal(4),
-        stressTestInterestRate: new Prisma.Decimal(6),
       },
     });
-  } else {
-    console.error('Error: Unable to find Company 1 or Admin user to associate StressTest');
   }
+  // Seed StressTest model with dummy data
+//   const company1 = await prisma.company.findUnique({ where: { name: 'Company 1' } });
+//   const adminUser = await prisma.user.findUnique({ where: { email: 'admin@gmail.com' } });
+
+//   if (company1 && adminUser) {
+//     await prisma.stressTest.create({
+//       data: {
+//         companyId: company1.id,
+//         userId: adminUser.id,
+//         investmentAmount: 1000000,
+//         interestRate: new Prisma.Decimal(5),
+//         interestRateDrop: new Prisma.Decimal(2),
+//         impactedYears: 5,
+//         reinvestmentPercentage: new Prisma.Decimal(0.02),
+
+//         investmentRate: new Prisma.Decimal(0.03),
+//         investmentRateDrop: new Prisma.Decimal(0.01),
+
+//         expensesAndYear: JSON.stringify({ year2023: 50000, year2024: 55000 }),
+//         increasePercentage: new Prisma.Decimal(0.05),
+
+//         loanPeriod: 10,
+//         baselineInterestRate: new Prisma.Decimal(4),
+//         stressTestInterestRate: new Prisma.Decimal(6),
+//       },
+//     });
+//   } else {
+//     console.error('Error: Unable to find Company 1 or Admin user to associate StressTest');
+//   }
 }
 
 main()
